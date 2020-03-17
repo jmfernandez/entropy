bf789c50a22a351c17602d0e24a8c3aeba7d441b
José María Fernández <josemariafg@gmail.com>
2020-03-18 00:56:31 +0100

    Added middle_entropy.py, which computes the entropy of the central 1MB

--- middle_entropy.py
+++ middle_entropy.py
@@ -2,12 +2,17 @@
 # -*- encoding: utf8 -*-
 '''
 Calculate Shannon Entropy (min bits per byte-character)
+of the middle 1MB section of a file, so there is an upper limit in
+time processing
 original source: https://www.kennethghartman.com/calculate-file-entropy/
 '''
 
-__version__ = '0.2'
-__description__ = 'Calculate Shannon Entropy for given files'
+from __future__ import division
 
+__version__ = '0.2'
+__description__ = 'Calculate Shannon Entropy for middle 1MB section of given file'
+
+import os
 import sys
 import math
 
@@ -16,31 +21,37 @@ BLOCKSIZE=1024*1024
 def main():
     if len(sys.argv) > 1:
         for filename in sys.argv[1:]:
-            entropy(filename,BLOCKSIZE)
+            middle_entropy(filename,BLOCKSIZE)
     else:
         print("Usage: {} <filename>+".format(sys.argv[0]), file=sys.stderr)
 
-def entropy(filename,blocksize):
+def middle_entropy(filename,blocksize):
     print('Opening file {}...'.format(filename))
+    
+    # First, file size
+    fileSize = os.stat(filename).st_size
+    
     counts = dict.fromkeys(range(256),0)
+    readSize = 0
     with open(filename, 'rb') as f:
+        # Second, locate seek position
+        if fileSize > blocksize:
+            f.seek((fileSize - blocksize) // 2)
+        
         byteArr = f.read(blocksize)
-        fileSize = 0
-        while len(byteArr) > 0:
-            fileSize += len(byteArr)
+        readSize = len(byteArr)
+        if readSize > 0:
             for byte in byteArr:
                 counts[byte] += 1
-            
-            byteArr = f.read(blocksize)
     
     print()
     print('File size in bytes: {:,d}'.format(fileSize))
     # calculate the frequency of each byte value in the file
-    print('Calculating Shannon entropy of file. Please wait...')
+    print('Calculating Shannon entropy of middle {} bytes file. Please wait...'.format(readSize))
     freqList = []
     for b in range(256):
         ctr = counts[b]
-        freqList.append(float(ctr) / fileSize)
+        freqList.append(float(ctr) / readSize)
     # Shannon entropy
     ent = 0.0
     for freq in freqList:
